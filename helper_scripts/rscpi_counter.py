8d9e632124b3735bc65b9fc2f40276264c4bf4c7
sakars <o.putaans@gmail.com>
2023-12-29 12:21:07 +0200

    modified helper script

--- helper_scripts/rscpi_counter.py
+++ helper_scripts/rscpi_counter.py
@@ -5,14 +5,19 @@ import time
 url = 'http://rscpi.local/exec'
 data = {
 	'command': '',
-	'expect_response': False,
+	'expect_response': True,
+    "response_timeout": 3000
 }
 
-for i in range(0, 10):
-	data['command'] = f'DISPLAY:TEXT \'TESTING: {i}/10\'\n'
-	r = requests.post(url, data=json.dumps(data))
-	time.sleep(2)
-
-data['command'] = 'DISPLAY:TEXT \'Done\'\n'
-
+data['command']= '*IDN?\n'
 r = requests.post(url, data=json.dumps(data))
+print(r.text)
+
+# for i in range(0, 5):
+# 	data['command'] = f'DISPLAY:TEXT \'TESTING: {i}/10\'\n'
+# 	r = requests.post(url, data=json.dumps(data))
+# 	time.sleep(1)
+
+# data['command'] = 'DISPLAY:TEXT \'Done\'\n'
+
+# r = requests.post(url, data=json.dumps(data))
